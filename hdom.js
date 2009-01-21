852690b27a2b1511de686a95a5373b5ac8f8effb
erik <erikeldridge@yahoo.com>
2009-01-20 23:57:05 -0800

    made walk more generic & simplified hdom fn

--- hdom.js
+++ hdom.js
@@ -1,73 +1,34 @@
-// a very simple method to generate a hash table representation of a dom tree
-var hashdom = function(root){
-	var ignorable = function(node){//@ref http://developer.mozilla.org/en/Whitespace_in_the_DOM
-		return ( node.nodeType == 8 ) || //returns true for a comment node or 
-			( (node.nodeType == 3) && //a text node that's all
-				!(/[^\t\n\r ]/.test(node.data)) ); //whitespace 
-	},
-	table = {},
-	index = {},
-	walk = function(node, hash){
-		var name = node.nodeName,
-			test,
-			i;
-		if(!index[hash + name]){
-			index[hash + name] = 0;
-		}
-		test = hash + name + index[hash + name];
-		if(table[test]){//test if hash already exists
-			index[hash + name] += 1;//if so, increment index for that hash val
-		}
-		hash += name + index[hash + name];
-		table[hash] = node;
-		for(i = 0; i < node.childNodes.length; i++){
-			if(!ignorable(node.childNodes[i])){
-				walk(node.childNodes[i], hash);
-			}
-		}
-	},
-	get = function(hash){
-		return table[hash];
-	},
-	add = function(child, hash){
-		var parent = table[hash],
-			name = child.nodeName,
-			test;
-		if(!index[hash + name]){
-			index[hash + name] = 0;
-		}
-		test = hash + name + index[hash + name];
-		if(table[test]){//test if hash already exists
-			index[hash + name]++;//if so, increment index for that hash val
-		}
-		hash += name + index[hash + name];
-		table[hash] = child;
-		parent.appendChild(child);
-	},
-	del = function(childHash, parentHash){
-		var parent = table[parentHash],
-			child = table[childHash],
-			name = child.nodeName,
-			i;
-		for(i = 0; i < child.childNodes.length; i++){//remove all descendent entries from table
-			del(childHash + child.childNodes[i].nodeName + index[childHash + child.childNodes[i].nodeName], 
-				childHash);
-		}
-		parent.removeChild(child);
-		table[childHash] = null;
-		if(0 > index[parentHash + name] - 1){
-			index[parentHash + name] = null;
-		}else{
-			index[parentHash + name]--;
+var hdom = function(root){
+	var table = {},
+		index = {},
+	walk = function(node, callback, val, depth){
+		val = val || '';
+		depth = depth || 0;
+		val = callback(node, val, depth);
+		for(var i = 0; i < node.childNodes.length; i++){
+			walk(node.childNodes[i], callback, val, depth + 1);
 		}
 	};
 	//init
-	walk(root, '');
+	walk(root, function(node, val, depth){
+		if( node.nodeType == 8  || ( (node.nodeType == 3) && !(/[^\t\n\r ]/.test(node.data)) ) ){
+			return '';
+		}
+		if(undefined === index[val + node.nodeName]){
+			index[val + node.nodeName] = 0;
+		}else{
+			index[val + node.nodeName]++;
+		}
+		// var hash = val + node.nodeName + index[val + node.nodeName];//<-- simple version
+		if(val){//<-- increased readability version
+			var hash = val + '_' + node.nodeName.toLowerCase() + index[val + node.nodeName];
+		}else{
+			var hash = node.nodeName.toLowerCase() + index[val + node.nodeName];
+		}
+		table[hash] = node;
+		return hash;
+	});
 	return {
-		'table':table,
-		'index':index,
-		'get':get,
-		'add':add,
-		'del':del
+		'table':table
 	};
 };
