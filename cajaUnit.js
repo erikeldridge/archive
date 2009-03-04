00a2bd94d9d04c221aa66897c91ae19aeca55edd
erik <erikeldridge@yahoo.com>
2009-03-04 00:48:18 -0800

    this is just the raw js for the unit test.  For example usage, please refer to cajaunit.html

---
+++ cajaUnit.js
@@ -0,0 +1,73 @@
+var cajaUnit = function(){
+    var foreach = function(collection, callback){
+    	if(collection.length){//array or node list
+    		for(var i = 0; i < collection.length; i++){
+    			callback(i, collection[i]);
+    		}
+    	}else if(collection.hasOwnProperty){
+    		for(var key in collection){
+    			if(collection.hasOwnProperty(key)){
+    				callback(key, collection[key]);
+    			}
+    		}
+    	}else{
+    		throw('foreach() error: collection (' + collection + ') is neither an array nor an object');
+    	}
+    },
+	sprintf = function (/*format, arg1, arg2  ...*/) {
+		var params = Array.prototype.slice.apply(arguments),//cache arguments quasi-array as actual array.  ref: http://www.hunlock.com/blogs/Functional_Javascript#quickIDX11
+			format = params[0],
+			args = params.slice(1),
+			pieces = format.split('%%');
+		foreach(args, function (index, arg) {
+			if (!arg) {
+				throw('sprintf() error: expecting string, but received'+arg+', which is a'+typeof arg+'Context: '+params.toString());
+			} else {
+				pieces[index] = pieces[index] + arg.toString();
+			}
+		});
+		return pieces.join('');
+	};
+	return {
+    	'createSuite':function(settings){
+    	    //default settings
+    		settings.outputId = 'output';
+    		settings.passClassName = 'pass';
+    		settings.failClassName = 'fail';
+    		var tests = [];
+            return {
+                'addTest':function(fn){
+    				foreach(settings, function(settingName, settingValue){
+    					fn.settings[settingName] = settingValue;
+    				});
+    				tests.push(fn);
+    			},
+    			'run':function(){
+    				foreach(tests, function(i, test){
+    					test.run();
+    				});
+    			}
+            };
+        },
+        'createTest':function(settings){
+    		return {
+    			'settings':settings,
+    			'run':function(){
+    				if(settings.setUp){
+    					var setUpResults = settings.setUp();
+    				}
+    				if(settings.test(setUpResults)){
+    					var html = sprintf('<div class="%%">%%: %%</div>', settings.passClassName, settings.suiteName, settings.testName);
+    					document.getElementById(settings.outputId).innerHTML += html;
+    				}else{console.log(settings);
+    					var html = sprintf('<div class="%%">%%: %%</div>', settings.failClassName, settings.suiteName, settings.testName);
+    					document.getElementById(settings.outputId).innerHTML += html;
+    				}
+    				if(settings.tearDown){
+    					settings.tearDown(setUpResults);
+    				}
+    			}
+    		};
+    	}
+    };
+}();
