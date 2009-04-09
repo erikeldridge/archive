8576c2803d915ef0732c15e2977fa4664f3be71c
erik <erikeldridge@yahoo.com>
2009-04-09 11:31:34 -0700

    - added usage notes - added license directly into file for clarity

--- custom_friend_selector.php
+++ custom_friend_selector.php
@@ -1,4 +1,35 @@
 <?php
+/*
+Purpose: 
+Demonstrate how to create a friend selector that's caja-safe
+
+Usage:
+- upload this file to your server
+- download Yahoo! PHP SDK (http://developer.yahoo.com/social/sdk/), if you don't have it already
+- adjust Yahoo.inc include to reflect path to SDK
+- create YAP application
+- set the app url to this file on your server
+- create config.inc file and define YAP key and secret using values from YAP app definition
+- preview the YAP app
+- click on the left side of the rectangle with the black border to set the focus on the input box
+- start typing a connection's nickname.  If you don't know your connections' nicknames, visit your Yahoo! profile and view your connections.  Their nicknames appear below their pictures.
+- Click on a name that appears below the input box to add its associated guid to the hidden "guids" input field in the form.  The clicked name will disappear form the dropdown and appear to the left of the input box
+- To remove a name (that's been added), click on it to the left of the input box
+- click the submit button to send the form data.  Note: the form submits to itself.  Assuming you added some names, you will see their guids printed at the top of the page.  Serving suggestions: you can plug these guids directly into a yml:name tag to get their names or a yml:profile-pic tag to see their pic, request data for a guid using Yahoo!'s social APIs (http://developer.yahoo.com/social/social), etc.
+
+License:
+Software License Agreement (BSD License)
+Copyright (c) 2009, Erik Eldridge.
+All rights reserved.
+
+Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* The name Erik Eldridge may not be used to endorse or promote products derived from this software without specific prior written permission of Erik Eldridge.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 require('config.inc');
 require('yosdk/Yahoo.inc');
 $session = YahooSession::requireSession(KEY, SECRET);
