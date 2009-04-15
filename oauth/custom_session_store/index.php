91f754f9d59277e81e15d51d9877b807d0346ade
erik <erikeldridge@yahoo.com>
2009-04-14 18:05:24 -0700

    now defining callback url in config

--- oauth/custom_session_store/index.php
+++ oauth/custom_session_store/index.php
@@ -2,11 +2,10 @@
 require_once("../../yosdk/Yahoo.inc");
 require_once("CustomSessionStore.inc");
 require_once("config.inc");
-$redirect = TRUE; 
-$callback = ''; 
-$guid = '{guid}';//set by app logic
+$redirect = TRUE;  
+$guid = 'BG5BMUK24OOYGHWKTJBCX2TN5E';//'{guid}';//set by app logic
 $sessionStore = new CustomSessionStore($guid);
-$session = YahooSession::initSession(KEY, SECRET, APPID, $redirect, $callback, $sessionStore);
+$session = YahooSession::initSession(KEY, SECRET, APPID, $redirect, CALLBACK, $sessionStore);
 ?>
 <pre>
 <? print_r($session->accessToken) ?>
