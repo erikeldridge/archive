4d88c573a490a0318a79f2f50f6b42b756bb8296
erik <erikeldridge@yahoo.com>
2009-04-14 18:15:34 -0700

    removed test guid

--- oauth/custom_session_store/index.php
+++ oauth/custom_session_store/index.php
@@ -3,7 +3,7 @@ require_once("../../yosdk/Yahoo.inc");
 require_once("CustomSessionStore.inc");
 require_once("config.inc");
 $redirect = TRUE;  
-$guid = 'BG5BMUK24OOYGHWKTJBCX2TN5E';//'{guid}';//set by app logic
+$guid = '{guid}';//set by app logic
 $sessionStore = new CustomSessionStore($guid);
 $session = YahooSession::initSession(KEY, SECRET, APPID, $redirect, CALLBACK, $sessionStore);
 ?>
