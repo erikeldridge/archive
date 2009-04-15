ba0d3b186dabdbe07e4f6122e37406a5ac2ff3f9
erik <erikeldridge@yahoo.com>
2009-04-14 17:35:20 -0700

    minor changes

--- oauth/custom_session_store/index.php
+++ oauth/custom_session_store/index.php
@@ -1,13 +1,12 @@
 <?php 
-require_once("yosdk/Yahoo.inc");
+require_once("../../yosdk/Yahoo.inc");
 require_once("CustomSessionStore.inc");
 require_once("config.inc");
-$applicationId = NULL;
 $redirect = TRUE; 
 $callback = ''; 
 $guid = '{guid}';//set by app logic
 $sessionStore = new CustomSessionStore($guid);
-$session = YahooSession::initSession(KEY, SECRET, $applicationId, $redirect, $callback, $sessionStore);
+$session = YahooSession::initSession(KEY, SECRET, APPID, $redirect, $callback, $sessionStore);
 ?>
 <pre>
 <? print_r($session->accessToken) ?>
