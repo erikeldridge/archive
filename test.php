a12c0517d09b702b8ecd7f31e751c8cb6639f986
pandayak <pandayak@Macintosh-68.local>
2009-07-15 23:11:42 -0700

    added comments describing how to run tests.  added code for responses that i forgot to copy over for the initial checkin.  fixed some minor typos

--- test.php
+++ test.php
@@ -1,57 +1,65 @@
 <?php
+/*
+This file will make a series of curl requests to itself using the Curl class when run as described below.
+
+How to run:
+- place this file in a location where it can be rendered by php and served to a browser, eg on your server
+- redefine $url to point to the location of this file, eg http://localhost/test.php
+- run this script by browsing to it and appending a 'run' parameter to the url, eg http://localhost/test.php?run
+*/
+$url = 'http://localhost/~pandayak/curl/test.php';
 if(isset($_GET['run'])){
-	require('Curl.inc');
+	require('curl.php');
 	$c = new Curl;
-	
 	//test default behavior
 	$param = 'qwe';
-	$test = $c->get('http://localhost/~pandayak/test/test.php', array('param' => $param));
+	$test = $c->get($url, array('param' => $param));
 	assert($param == $test);
 	
 	$param = 'asd';
-	$test = $c->post('http://localhost/~pandayak/test/test.php', array('param' => $param));
+	$test = $c->post($url, array('param' => $param));
 	assert($param == $test);
 	
 	$param = 'zxc';
-	$test = $c->put('http://localhost/~pandayak/test/test.php', array('param' => $param));
+	$test = $c->put($url, array('param' => $param));
 	assert($param == $test);
 	
 	$param = '123';
-	$test = $c->delete('http://localhost/~pandayak/test/test.php', array('param' => $param));
+	$test = $c->delete($url, array('param' => $param));
 	assert($param == $test);
 	
 	//test multi behavior w/ single requests
 	$param = 'qwe';
 	$test = $c->multi(array(
-		array('get', 'http://localhost/~pandayak/test/test.php', array('param' => $param))
+		array('get', $url, array('param' => $param))
 	));
 	assert($param== $test[0]);
 	
 	$param = 'asd';
 	$test = $c->multi(array(
-		array('post', 'http://localhost/~pandayak/test/test.php', array('param' => $param))
+		array('post', $url, array('param' => $param))
 	));
 	assert($param == $test[0]);
 	
 	$param = 'zxc';
 	$test = $c->multi(array(
-		array('put', 'http://localhost/~pandayak/test/test.php', array('param' => $param))
+		array('put', $url, array('param' => $param))
 	));
 	assert($param == $test[0]);
 	
 	$param = '123';
 	$test = $c->multi(array(
-		array('delete', 'http://localhost/~pandayak/test/test.php', array('param' => $param))
+		array('delete', $url, array('param' => $param))
 	));
 	assert($param == $test[0]);
 	
 	//test multi requests w/ multiple requests
 	$params = array('qwe', 'asd', 'zxc', '123');
 	$test = $c->multi(array(
-		array('get', 'http://localhost/~pandayak/test/test.php', array('param' => $params[0])),
-		array('post', 'http://localhost/~pandayak/test/test.php', array('param' => $params[1])),
-		array('put', 'http://localhost/~pandayak/test/test.php', array('param' => $params[2])),
-		array('delete', 'http://localhost/~pandayak/test/test.php', array('param' => $params[3]))
+		array('get', $url, array('param' => $params[0])),
+		array('post', $url, array('param' => $params[1])),
+		array('put', $url, array('param' => $params[2])),
+		array('delete', $url, array('param' => $params[3]))
 	));
 	
 	assert($params[0] == $test[0]);
@@ -65,7 +73,26 @@ if(isset($_GET['run'])){
 	
 	//original test content
 	//require 'curl.php';
-	//$curl = new Curl;
+	$curl = new Curl;
 
 	print_r($curl->get('google.com')->headers);
+}
+
+switch($_SERVER['REQUEST_METHOD']){
+	case 'GET':
+		echo $_GET['param'];
+		break;
+	case 'POST':
+		echo $_POST['param'];
+		break;
+	case 'PUT':
+		$str = file_get_contents("php://input");
+		parse_str($str, $input);
+		echo $input['param'];
+		break;
+	case 'DELETE':
+		$str = file_get_contents("php://input");
+		parse_str($str, $input);
+		echo $input['param'];
+		break;
 }
