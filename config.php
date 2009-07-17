d4d101e1d312f6d509a90af472c431e4c3b10352
Jonathan LeBlanc <joncleblanc@yahoo.com>
2009-07-17 13:25:14 -0700

    PHP OAuth flow with custom session store

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('KEY',"dj0yJmk9d1k5dUlLQzNXUkVnJmQ9WVdrOVQyTjJTMjlLTm1jbWNHbzlNVFU0TmpJeE1UWTJOQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1iNA--");
+define('SECRET',"6cee8ab94a80f12ba734f3f5d1ef624aeec38689");   
+define('APPID',"OcvKoJ6g");
+define('CALLBACK',"http://www.liquidmediatech.com");
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+require_once("config.php");
+require_once("php_sdk/Yahoo.inc");
+require_once("CustomSessionStore.inc");
+
+//capture POST data for update
+/*$title = $_POST['title'];
+$description = $_POST['description'];
+$link = $_POST['link'];
+$token = $_POST['token'];*/
+
+$title = "test";
+$description = "test description";
+$link = "http://nakedtechnologist.com";
+
+//initialize session
+$sessionStore = new CustomSessionStore();
+$session = YahooSession::initSession(KEY, SECRET, APPID, TRUE, CALLBACK, $sessionStore);
+$yahoo_user = $session->getSessionedUser();
+
+//create new update
+if ($title && $description && $link){
+	$suid = $yahoo_user->guid . time();
+	$yahoo_user->insertUpdate($suid, $title, $link, $description);
+	echo "Update Successful";
+}
+?>
