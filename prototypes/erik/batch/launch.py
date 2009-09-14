936d152d16fcdc0c9aa3f540190032c092cdc707
erik <erikeldridge@yahoo.com>
2009-09-14 00:29:35 -0700

    created 'prototypes' dir to house our proto architectures. added mine as initial commit

---
+++ prototypes/erik/batch/launch.py
@@ -0,0 +1,10 @@
+"""
+purpose
+* launch and monitor batch process cluster
+
+notes
+* this file may just need to be a set of instructions for running Cloudera's cluster management script (http://www.cloudera.com/hadoop-ec2-ebs-beta)
+
+process
+* configure and launch cluster based on Cloudera's docs
+"""
