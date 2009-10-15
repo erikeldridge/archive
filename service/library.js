573aba865124ea2f0ee2dcde5a9da8066dc91d72
erik <erikeldridge@yahoo.com>
2009-10-14 21:53:47 -0700

    now wrapping all functionality in a container obj to simplify ui

--- service/library.js
+++ service/library.js
@@ -1,47 +1,115 @@
-var totalQtyChunks = qtyChunksCollected = chunkStr = null,
-    completeCallback = function (data) {
-        console.log(data);
-    },
-    iframeCallback = function (chunk) {
-    
-        //validate input
-        if('string' !== typeof chunk){
-           return;
-        }
+var sdk = (function () {
+		var totalQtyChunks = qtyChunksCollected = chunkStr = iframe = null,
+			completeCallback = function (data, fn) {
+		        fn(data);
+		    },
+		
+			//todo: make iframe callback name dynamic
+		    iframeCallback = function (chunk) {
 
-        //Count defined by 1st iframe.
-        //Guard against chunkStr w/ 'totalQtyChunks' str in them.
-        //initialize affiliate vars here so we can reuse
-        if(!totalQtyChunks && -1 !== chunk.indexOf('totalQtyChunks')){
-            totalQtyChunks = Number(chunk.substr(chunk.indexOf('=') + 1));
-            qtyChunksCollected = 0;
-            chunkStr = '';
+		        //validate input
+		        if('string' !== typeof chunk){
+		           return;
+		        }
 
-        //Chunks are passed to subsequent iframes.
-        }else if (totalQtyChunks) {
-            chunkStr += chunk;
-            qtyChunksCollected++;
-        
-            //if qtyChunksCollected === totalQtyChunks, we're done
-            if (qtyChunksCollected === totalQtyChunks) {
+		        //Count defined by 1st iframe.
+		        //Guard against chunkStr w/ 'totalQtyChunks' str in them.
+		        //initialize affiliate vars here so we can reuse
+		        if(!totalQtyChunks && -1 !== chunk.indexOf('totalQtyChunks')){
+		            totalQtyChunks = Number(chunk.substr(chunk.indexOf('=') + 1));
+		            qtyChunksCollected = 0;
+		            chunkStr = '';
 
-                //decode and cache data
-               var data = decodeURIComponent(chunkStr);
+		        //Chunks are passed to subsequent iframes.
+		        }else if (totalQtyChunks) {
+		            chunkStr += chunk;
+		            qtyChunksCollected++;
 
-               //do something
-               completeCallback(data);
+		            //if qtyChunksCollected === totalQtyChunks, we're done
+		            if (qtyChunksCollected === totalQtyChunks) {
 
-               //clear affiliate vars for next use
-               totalQtyChunks = qtyChunksCollected = chunkStr = null;
-            }
-        }
-    },
-    iframe = document.createElement('iframe');
+		                //decode and cache data
+		               var data = decodeURIComponent(chunkStr);
 
-//init iframe so browser caches file
-iframe.src = 'iframe.html';
-iframe.style.display = 'none';
-document.body.appendChild(iframe);
+		               //do something
+		               completeCallback(data, console.log);
 
-//make service call
-iframe.src = 'service/?method=profile&hash=asd123';
+		               //clear affiliate vars for next use
+		               totalQtyChunks = qtyChunksCollected = chunkStr = null;
+		            }
+		        }
+		    };
+		    //iframe = document.createElement('iframe');
+		return {
+			'request' : function (params, fn) {
+				var src = 'service';
+				if (params) {
+					//todo: format params
+				}
+				iframe.src = src;
+				if (fn) {
+					completeCallback = fn;
+				}
+			},
+			'query' : function (yql) {
+				
+			},
+			'iframeCallback' : iframeCallback,
+			'init' : function () {
+				iframe = document.createElement('iframe');
+				iframe.src = 'iframe.html';//todo: make dynamic
+				iframe.style.display = 'none';
+				document.body.appendChild(iframe);
+				this.request();
+			}
+		};
+	}());
+	
+//todo: set hash in here
+sdk.init();
+
+// var totalQtyChunks = qtyChunksCollected = chunkStr = null,
+//     completeCallback = function (data) {
+//         console.log(data);
+//     },
+//     iframeCallback = function (chunk) {
+//     
+//         //validate input
+//         if('string' !== typeof chunk){
+//            return;
+//         }
+// 
+//         //Count defined by 1st iframe.
+//         //Guard against chunkStr w/ 'totalQtyChunks' str in them.
+//         //initialize affiliate vars here so we can reuse
+//         if(!totalQtyChunks && -1 !== chunk.indexOf('totalQtyChunks')){
+//             totalQtyChunks = Number(chunk.substr(chunk.indexOf('=') + 1));
+//             qtyChunksCollected = 0;
+//             chunkStr = '';
+// 
+//         //Chunks are passed to subsequent iframes.
+//         }else if (totalQtyChunks) {
+//             chunkStr += chunk;
+//             qtyChunksCollected++;
+//         
+//             //if qtyChunksCollected === totalQtyChunks, we're done
+//             if (qtyChunksCollected === totalQtyChunks) {
+// 
+//                 //decode and cache data
+//                var data = decodeURIComponent(chunkStr);
+// 
+//                //do something
+//                completeCallback(data);
+// 
+//                //clear affiliate vars for next use
+//                totalQtyChunks = qtyChunksCollected = chunkStr = null;
+//             }
+//         }
+//     },
+//     iframe = document.createElement('iframe');
+// 
+// //init iframe so browser caches file
+// 
+// 
+// //make service call
+// iframe.src = 'service/?method=profile&hash=asd123';
