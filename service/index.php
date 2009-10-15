f07149d27ce4416e9495aa2dbd92d092d63ea857
erik <erikeldridge@yahoo.com>
2009-10-14 23:58:52 -0700

    holy smokes.  waterfall reorganizing of  input validation logic

--- service/index.php
+++ service/index.php
@@ -1,56 +1,77 @@
 <?php
 //safely fetch input
 $filters = array(
-    'method' => FILTER_SANITIZE_STRING,
+    'serviceId' => FILTER_SANITIZE_STRING,
     'hash' => FILTER_SANITIZE_STRING,
-    'time' => FILTER_SANITIZE_STRING,
     'crumb' => FILTER_SANITIZE_STRING
 );
 $input = filter_var_array($_GET, $filters);
 
-switch($input['method']){
-    case 'profile':
-        //todo: validate crumb
-        
-        //fetch credentials from store
-        $credentials = include('credentials.php');
-        
-        //validate hash
-        if(!isset($credentials[$input['hash']])){
-           //die('invalid hash');
-        }
-        
-        list($key, $secret) = $credentials[$input['hash']];
-        
-        //call api using key/secret for profile data
-        $data = json_encode(array(
-            'name' => 'foo bar',
-            'address' => '123 first st.',
-            'city' => 'burlingame',
-            'state' => 'ca',
-            'country' => 'usa'
-        ));
-        break;
-        
-    case 'foo':
-        
-        break;
-        
-    default:
-        
-        //require hash on initial req
-        if(!isset($input['hash'])){
-            $data = json_encode(array('error'=>"hash req'd for init request"));
-            break;
-        }
-    
-        //on initial call, provide crumb only
-        $crumb = md5($input['hash'].time());
-        $data = json_encode(array('crumb'=>$crumb));
-        break;
+//perform negative validation
+//hash is always required
+if(!isset($input['hash'])){
+    $data = json_encode(array('error'=>"hash req'd for all requests"));
 }
+
+//if service
+if(isset($input['serviceId'])){
+
+    if(!isset($input['crumb'])){
+        $data = json_encode(array('error'=>"crumb req'd for all service requests"));
+    }else{
+        //validate crumb using hash and crumb store
+        //if invalid crumb -> error
+    }
+    
+    //service endpoints
+    switch($input['serviceId']){
+
+        //some service requiring authentication, eg yql's social.profile table
+        case 'private':
+            //hash => services => {serviceId1 => {name, url, auth type, key (opt), secret (opt)}, serviceId2 => ...}
+
+            //fetch credentials from store
+            $store = include('store.php');
+
+            //validate hash
+            if(!isset($store[$input['hash']])){
+               //die('invalid hash');
+            }elseif(!isset($store[$input['serviceId']])){
+
+            }
+
+            list($key, $secret) = $store[$input['hash']];
+
+            //call api using key/secret for profile data
+            $data = json_encode(array(
+                'name' => 'foo bar',
+                'address' => '123 first st.',
+                'city' => 'burlingame',
+                'state' => 'ca',
+                'country' => 'usa'
+            ));
+            break;
+
+        case 'public':
+
+            break;
+
+        default:
+            //error: invalid service id
+            break;
+    }
+    
+//init
+}else{
+    $crumb = md5($input['hash'].time());
+    $data = json_encode(array('crumb'=>$crumb));
+}
+
+//format data for output
 $data = urlencode($data);
 $chunks = str_split($data, 10);
+
+//output markup
 ?>
 
 <iframe src="../iframe.html?totalQtyChunks=<?= count($chunks) ?>"></iframe>
