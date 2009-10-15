fe3775ac854f35cc0a415cac5cbacad9434ced86
erik <erikeldridge@yahoo.com>
2009-10-15 06:00:24 -0700

    now queueing a (single) request on init callback if crumb isn't ready

--- service/library.js
+++ service/library.js
@@ -4,9 +4,10 @@ var sdk = (function () {
 		var totalQtyChunks = null,
 			qtyChunksCollected = null,
 			chunkStr = null,
-			iframe = null,
+			iframes = [],
 			hash = null,
-			crumb = null,			
+			crumb = null,	
+			requestQueue = [],		
 			completeCallback = function (data) {
 				//This is a stub fn.  
 				//The user should define a callback as a param to request()
@@ -47,17 +48,30 @@ var sdk = (function () {
 		            }
 		        }
 		    },
+			initCallback = function () {
+				
+			},
 			request = function (paramsObj, fn) {
+				
+				if (!hash) {
+					throw('hash required for all requests');
+				}
 				
 				//todo: allow global def of service url
 				//every request requires hash so server can lookup acct
 				var src = 'service/?hash=' + hash;
 				
-				if (crumb) {
-					src += '&crumb=' + crumb;
-				}
-				
 				if (paramsObj) {
+					
+					if (!crumb) {
+						
+						//add request to init callback
+						console.log('adding req to queue');
+						requestQueue.push({'params':paramsObj, 'callback':fn});
+						return;
+					}
+					
+					src += '&crumb=' + crumb;
 					
 					//append params to req url
 					//todo: either use foreach or add hasOwnObject check js-good-stuff-style
@@ -65,6 +79,12 @@ var sdk = (function () {
 						src += '&' + key + '=' + paramsObj[key];
 					}
 				}
+				
+				//create iframe com channel
+				iframe = document.createElement('iframe');
+				iframe.src = 'iframe.html';//todo: make dynamic
+				iframe.style.display = 'none';
+				document.body.appendChild(iframe);
 				
 				//make req
 				iframe.src = src;
@@ -74,42 +94,44 @@ var sdk = (function () {
 					completeCallback = fn;
 				}
 			};
-
+			
 			//init
 			//fetch dev key case-hardened-js-style
 			var scriptNodes = document.getElementsByTagName('script');
 			for(var i = 0; i < scriptNodes.length; i++){
-				
+
 				//todo: make script src editable
 				if(-1 !== scriptNodes[i].src.indexOf('service/library.js')){
-					
+
 					//todo: verify we're trimming all whitespace
 					hash = scriptNodes[i].innerHTML.replace('\n', '');
+					hash = scriptNodes[i].innerHTML.replace(' ', '');
 				}
 			}
-			
+
 			//validate existence of hash
 			if (!hash) {
 				throw('hash required in sdk lib script include block');
 			}
-			
+
 			//create iframe com channel
 			iframe = document.createElement('iframe');
 			iframe.src = 'iframe.html';//todo: make dynamic
 			iframe.style.display = 'none';
 			document.body.appendChild(iframe);
 			
-			//fetch only crumb in 1st request
-			request({'hash':hash}, function(data){
-				//todo: cache crumb internally
+			//1st req is for fetching crumb
+			request(null, function (data) {
 				console.log(data);
 				
 				//static crumb for testing
 				crumb = '4dc49e30be212209c559e0a990d8f3f8';
+				
+				//if requests are made b4 crumb returns, requests are queued.  Now the crumb is ready, so exec queued requests
+				request(requestQueue[0].params, requestQueue[0].callback);
 			});
 			
 		return {
-			
 			//the main, general purpose util
 			'request' : request,
 			
