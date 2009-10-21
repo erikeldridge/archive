d5b2c42e4a191a3f8aef0fab7dd3701498f5b3b4
erik <erikeldridge@yahoo.com>
2009-10-21 17:37:37 +0900

    added field to store for access token

--- service/store.php
+++ service/store.php
@@ -2,7 +2,12 @@
 return array(
     //hash => services => {service1 => {name, url, auth type, key (opt), secret (opt)}, service2 => ...}
     'asd456' => array(
-        'privateco' => array('key'=>'qwe', 'secret'=>'123')),
+        'privateco' => array(
+            'key'=>'qwe', 
+            'secret'=>'123', 
+            'token'=>''
+        )
+    ),
     'qwe123' => array('baz', 'bark'),
 );
 ?>
