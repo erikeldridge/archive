5da43999b157dc0e6a128a542207c1e43fea12e7
erik <erikeldridge@yahoo.com>
2009-10-21 17:32:22 +0900

    updated example record to use new format: uid => [serviceid1 => [key, secret], serviceid2 => [...], ...]

--- service/store.php
+++ service/store.php
@@ -1,7 +1,8 @@
 <?php
-$credentials = array(
-    'asd456' => array('foo', 'bar'),
+return array(
+    //hash => services => {service1 => {name, url, auth type, key (opt), secret (opt)}, service2 => ...}
+    'asd456' => array(
+        'privateco' => array('key'=>'qwe', 'secret'=>'123')),
     'qwe123' => array('baz', 'bark'),
 );
-return $credentials;
 ?>
