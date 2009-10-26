6e7d3c7a7357bf815b2e3f5037fef782b3266ee4
erik <erikeldridge@yahoo.com>
2009-10-25 17:51:00 -0700

    minor change to comments

--- client/library.js
+++ client/library.js
@@ -1,27 +1,8 @@
-
-/**
- * @package http://github.com/erikeldridge/foxbatexample/tree/master    
- * @copyright (c) 2009, Erik Eldridge, all rights reserved
- * @license BSD Open Source License
- *
- *   Permission is hereby granted, free of charge, to any person obtaining a copy
- *   of this software and associated documentation files (the "Software"), to deal
- *   in the Software without restriction, including without limitation the rights
- *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
- *
- *   The above copyright notice and this permission notice shall be included in
- *   all copies or substantial portions of the Software.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *   THE SOFTWARE.
- **/
+/*
+Copyright (c) 2009, Erik Eldridge. All rights reserved.
+Code licensed under the BSD License:
+http://test.erikeldridge.com/foxbatexample/license.txt
+*/
 
 var sdk = function () {
 	var hash = null,
@@ -29,6 +10,8 @@ var sdk = function () {
 		request = function (params, userCallback) {
 			var iframe = document.createElement('iframe'),
 				id = null,
+				
+				//this should be populated by service
 				url = 'http://test.erikeldridge.com/foxbatexample',
 				chunks = [],
 				collected = 0,
