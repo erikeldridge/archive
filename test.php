e77e99d0849d0a6863a0371f5ce67dd41fc658de
erik <erikeldridge@yahoo.com>
2009-10-25 23:12:45 -0700

    added test for update

--- test.php
+++ test.php
@@ -3,14 +3,23 @@ require 'sdk.php';
 require 'secure.inc';
 $netdb = new Netdb($netdbUid, $netdbSecret);
 
+//test set
 $key = 'asd123';
 $value = 'thisthat';
 $response = $netdb->set($key, $value);
 assert('success' == $response->status);
 assert('thisthat' == $response->value);
 
+//test get
 $key = 'asd123';
 $response = $netdb->get($key);
 assert('success' == $response->status);
 assert('thisthat' == $response->value);
+
+//test update
+$key = 'asd123';
+$value = 'fooboo';
+$response = $netdb->set($key, $value);
+assert('success' == $response->status);
+assert('fooboo' == $response->value);
 ?>
