a549b78f9cd7a700b2c0191c00120f4d2076c5d3
erik <erikeldridge@yahoo.com>
2009-10-28 05:46:26 -0700

    updated tests to validate changes made to api.php and register.php

--- test.php
+++ test.php
@@ -7,13 +7,14 @@ http://github.com/erikeldridge/authproxy/blob/master/license.txt
 */
 
 require 'keydb.php';
+require '../netdb/sdk.php';
 
 //http://github.com/shuber/curl
 require '../curl/curl.php';
 $curl = new Curl;
 
-//insert oauth record
-$userId = '1';
+//BEGIN: insert oauth record
+$userId = 'BG5BMUK24OOYGHWKTJBCX2TN5E';
 $providerName = 'yahoo.com';
 $consumerKey = 'sjdfkjsjdlkfjl';
 $consumerSecret = 'jljsldjfjsldjfljsjdf';
@@ -33,24 +34,45 @@ $response = json_decode($curl->post($url, $params)->body);
 assert('success' == $response->status);
 assert(isset($response->recordId) && !empty($response->recordId));
 
-//get oauth record
+//audit service record in db
+$storage = new Netdb(KeyDB::$netdb_key, KeyDB::$netdb_secret);
 $recordId = $response->recordId;
-$url = sprintf('%s/authproxy/api.php', 'http://localhost/~eldridge');
-$params = array(
-    'hash' => sha1(KeyDB::$credentials[$userId].$userId),
-    'userId' => $userId,
-    'type' => 'oauth',
-    'recordId' => $recordId
-);
-$response = json_decode($curl->get($url, $params)->body);
-assert('success' == $response->status);
+
+$storageKey = sprintf('authproxy-services-%s-%s', $userId, $recordId);
+$response = $storage->get($storageKey);
 $value = json_decode($response->value);
 assert($value->providerName == $providerName);
 assert($value->consumerKey == $consumerKey);
 assert($value->consumerSecret == $consumerSecret);
 assert($value->callbackUrl == $callbackUrl);
 
-//delete record
+//audit user record in db
+$storageKey = sprintf('authproxy-users-%s', $userId);
+$response = $storage->get($storageKey);
+$value = json_decode($response->value);
+assert(in_array($recordId, $value->recordIds));
+
+//END: insert oauth record
+
+//get oauth record
+$url = sprintf('%s/authproxy/api.php', 'http://localhost/~eldridge');
+$params = array(
+    'hash' => sha1(KeyDB::$credentials[$userId].$userId),
+    'userId' => $userId,
+    'type' => 'oauth',
+    
+    //record id defined on insertion
+    'recordId' => $recordId
+);
+
+$response = json_decode($curl->get($url, $params)->body);
+assert('success' == $response->status);
+assert($response->value->providerName == $providerName);
+assert($response->value->consumerKey == $consumerKey);
+assert($response->value->consumerSecret == $consumerSecret);
+assert($response->value->callbackUrl == $callbackUrl);
+
+//BEGIN: delete record
 $url = sprintf('%s/authproxy/api.php', 'http://localhost/~eldridge');
 $params = array(
     'hash' => sha1(KeyDB::$credentials[$userId].$userId),
@@ -59,6 +81,20 @@ $params = array(
     'type' => 'oauth',
     'recordId' => $recordId
 );
-$response = json_decode($curl->post($url, $params)->body);
+$response = json_decode($curl->post($url, $params)->body);var_dump($response);
 assert('success' == $response->status);
+
+//audit service record in db
+$storageKey = sprintf('authproxy-services-%s-%s', $userId, $recordId);
+$response = $storage->get($storageKey);
+$value = json_decode($response->value);
+assert(empty($value));
+
+//audit user record in db
+$storageKey = sprintf('authproxy-users-%s', $userId);
+$response = $storage->get($storageKey);
+$value = json_decode($response->value);echo "$recordId<br/>";
+assert(!in_array($recordId, $value->recordIds));
+//BEGIN: delete record
+
 ?>
