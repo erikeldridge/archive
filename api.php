4d8932afdc2835ff8877cc05da88bd36df11ae3e
erik <erikeldridge@yahoo.com>
2009-10-28 05:44:33 -0700

    on creation of new record, now adding record id to user record so we can fetch all records for a user later

--- api.php
+++ api.php
@@ -22,6 +22,7 @@ $filters = array(
     'action' => FILTER_SANITIZE_STRING,
     'userId' => FILTER_SANITIZE_STRING,
     'recordId' => FILTER_SANITIZE_STRING,
+    'type' => FILTER_SANITIZE_STRING,
     
     //oauth
     'providerName' => FILTER_SANITIZE_STRING,
@@ -37,7 +38,7 @@ if (!isset($input['userId'])) {
     $response = array('status' => 'error', 'details' => 'invalid userId: '.$input['userId']);
 } elseif(sha1(KeyDB::$credentials[$input['userId']].$input['userId']) != $input['hash']) {
     $response = array('status' => 'error', 'details' => 'invalid hash: '.$input['hash']);
-} elseif('oauth' == $input['type']) {
+} elseif('oauth' != $input['type']) {
     $response = array('status' => 'error', 'details' => 'invalid type: '.$input['type']);
 } elseif ('GET' != $_SERVER['REQUEST_METHOD'] && 'POST' != $_SERVER['REQUEST_METHOD']) {   
     $response = array('status' => 'error', 'details' => 'invalid request method: '.$_SERVER['REQUEST_METHOD']);
@@ -47,17 +48,16 @@ if (!isset($input['userId'])) {
     require '../netdb/sdk.php';
     $storage = new Netdb(KeyDB::$netdb_key, KeyDB::$netdb_secret);
     
-    $storageKey = sprintf('%s-%s-%s', $input['userId'], $input['type'], $input['recordId']);
+    $storageKey = sprintf('authproxy-services-%s-%s', $input['userId'], $input['recordId']);
     $response = $storage->get($storageKey);
 
     //confirm success
     if('success' == $response->status){
 
+        //storage returns success if it doesn't fail, but there may not be a record
         if (isset($response->value)) {
-            $response = array('value' => json_decode($response->value));
+            $response->value = json_decode($response->value);
         }
-        
-        $response['status'] = 'success';
         
     } else {
         $response = array('status' => 'error', 'debug' => $response);                
@@ -83,14 +83,29 @@ if (!isset($input['userId'])) {
             $obj->callbackUrl = $input['callbackUrl'];
             $storageValue = json_encode($obj);
         
-            //store obj
             $recordId = sha1($storageValue.time());
-            $storageKey = sprintf('%s-%s-%s', $input['userId'], $input['type'], $recordId);
+            
+            //add user id to record id just in case
+            $storageKey = sprintf('authproxy-services-%s-%s', $input['userId'], $recordId);
+            
             $response = $storage->set($storageKey, $storageValue);
         
+            //sanity check
+            if('success' != $response->status){
+                $response = array('status' => 'error', 'debug' => print_r($response, true));
+                break;            
+            }
+            
+            //add record id to credentials array in meta for user
+            $storageKey = sprintf('authproxy-users-%s', $input['userId']);
+            $response = $storage->get($storageKey);
+            $value = json_decode($response->value);
+            $value->recordIds[] = $recordId;
+            $response = $storage->set($storageKey, json_encode($value));
+            
             //confirm success
             if('success' == $response->status){
-                $response = array('status' => 'success', 'recordId' => $recordId, 'debug' => print_r($input, true));
+                $response = array('status' => 'success', 'recordId' => $recordId, 'debug' => print_r($response, true));
             } else {
                 $response = array('status' => 'error', 'debug' => $response);                
             }
@@ -106,7 +121,7 @@ if (!isset($input['userId'])) {
             $storageValue = json_encode($obj);
             
             //store obj
-            $storageKey = sprintf('%s-%s-%s', $input['userId'], $input['type'], $input['recordId']);
+            $storageKey = sprintf('authproxy-services-%s-%s', $input['userId'], $input['recordId']);
             $response = $storage->set($storageKey, $storageValue);
             
             //confirm success
@@ -121,12 +136,20 @@ if (!isset($input['userId'])) {
         case 'delete':
 
             //store empty obj
-            $storageKey = sprintf('%s-%s-%s', $input['userId'], $input['type'], $input['recordId']);
+            $storageKey = sprintf('authproxy-services-%s-%s', $input['userId'], $input['recordId']);
             $response = $storage->set($storageKey, '');
 
+            //remove record id from services array in meta for user
+            $storageKey = sprintf('authproxy-users-%s', $input['userId']);
+            $response = $storage->get($storageKey);
+            $value = json_decode($response->value);
+            $index = array_search($input['recordId'], $value->recordIds);
+            unset($value->recordIds[$index]);
+            $response = $storage->set($storageKey, json_encode($value));
+            
             //confirm success
             if('success' == $response->status){
-                $response = array('status' => 'success', 'debug' => $response);
+                $response = array('status' => 'success', 'debug' => print_r($value->recordIds, true));
             } else {
                 $response = array('status' => 'error', 'debug' => $response);                
             }
