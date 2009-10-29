557e42adfb86ccef635493ff2136150e131f225e
erik <erikeldridge@yahoo.com>
2009-10-29 06:56:33 -0700

    simplified url prep

--- sdk.php
+++ sdk.php
@@ -4,26 +4,14 @@ if (!class_exists('Curl'))) {
     throw new Exception('Curl class (http://github.com/shuber/curl) required');
 }
 
-class NetDB {
-    
-    //template api url
-    private $url = "http://test.erikeldridge.com/netdb/{uid}/{hash}/{key}";
-    
-    //str_replace search args (using str_replace because it's self-descriptive)
-    private $search = array('{uid}', '{hash}', '{key}');
-    
+class NetDB {    
     private $curl = new Curl;
     function __construct($uid, $secret){
-        $this->uid = $uid;
-        $this->secret = $secret;
-        $this->hash = sha1($secret.$uid);
+        $hash = sha1($secret.$uid);
+        $this->url = sprintf('http://test.erikeldridge.com/netdb/%s/%s/', $uid, $hash);
     }
     function get($key){
-        
-        //prep url
-        $replace = array($this->uid, $this->hash, $key);
-        $url = str_replace($this->search, $replace, $this->url);
-        
+        $url = $this->url.'/'.$key;        
         $response = $this->curl->get($url)->body;
         return json_decode($response);
     }
@@ -31,11 +19,7 @@ class NetDB {
         if(!is_string($value)){
            throw(new Exception('value must be a string, not: '.print_r($value, true)));
         }
-        
-        //prep url
-        $replace = array($this->uid, $this->hash, $key);
-        $url = str_replace($this->search, $replace, $this->url);
-        
+        $url = $this->url.'/'.$key;
         $params = array('value'=>$value);
         $response = $this->curl->post($url, $params)->body;
         return json_decode($response);
