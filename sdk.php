c3e7c74d83c51653a1c3f7ef30e62f2bdf6245db
erik <erikeldridge@yahoo.com>
2009-10-28 19:11:56 -0700

    added comments

--- sdk.php
+++ sdk.php
@@ -1,8 +1,13 @@
 <?php
 
 class NetDB {
+    
+    //template api url
     private $url = "http://test.erikeldridge.com/netdb/{uid}/{hash}/{key}";
+    
+    //str_replace search args (using str_replace because it's self-descriptive)
     private $search = array('{uid}', '{hash}', '{key}');
+    
     private $curl = new Curl;
     function __construct($uid, $secret){
         $this->uid = $uid;
@@ -10,8 +15,11 @@ class NetDB {
         $this->hash = sha1($secret.$uid);
     }
     function get($key){
+        
+        //prep url
         $replace = array($this->uid, $this->hash, $key);
         $url = str_replace($this->search, $replace, $this->url);
+        
         $response = $this->curl->get($url)->body;
         return json_decode($response);
     }
@@ -19,8 +27,11 @@ class NetDB {
         if(!is_string($value)){
            throw(new Exception('value must be a string, not: '.print_r($value, true)));
         }
+        
+        //prep url
         $replace = array($this->uid, $this->hash, $key);
         $url = str_replace($this->search, $replace, $this->url);
+        
         $params = array('value'=>$value);
         $response = $this->curl->post($url, $params)->body;
         return json_decode($response);
