39abfb87edeaf6b9dccf4ccd879c8182c88fc799
erik <erikeldridge@yahoo.com>
2009-10-30 17:18:52 -0700

    now passing base url into constructor for flexibility. no longer relying on mod rewrite rule for endpoint

--- sdk.php
+++ sdk.php
@@ -1,17 +1,17 @@
 <?php
 
-if (!class_exists('Curl'))) {
+if (!class_exists('Curl')) {
     throw new Exception('Curl class (http://github.com/shuber/curl) required');
 }
 
 class NetDB {    
-    private $curl = new Curl;
-    function __construct($uid, $secret){
-        $hash = sha1($secret.$uid);
-        $this->url = sprintf('http://test.erikeldridge.com/netdb/%s/%s/', $uid, $hash);
+    function __construct($url, $uid, $secret){
+        $this->curl = new Curl;
+        $hash = sha1($secret.$uid);       
+        $this->url = sprintf('%s/netdb/api.php?uid=%s&hash=%s&', $url, $uid, $hash);
     }
     function get($key){
-        $url = $this->url.'/'.$key;        
+        $url = $this->url.'key='.$key;        
         $response = $this->curl->get($url)->body;
         return json_decode($response);
     }
@@ -19,7 +19,7 @@ class NetDB {
         if(!is_string($value)){
            throw(new Exception('value must be a string, not: '.print_r($value, true)));
         }
-        $url = $this->url.'/'.$key;
+        $url = $this->url.'key='.$key; 
         $params = array('value'=>$value);
         $response = $this->curl->post($url, $params)->body;
         return json_decode($response);
