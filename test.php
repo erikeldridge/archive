251adef262047dec6ef264e7e16cd7080ee58b0d
erik <erikeldridge@yahoo.com>
2009-11-08 18:07:02 -0800

    - replaced references to keydb w/ local vars to simplify debugging - now using an sqlite-backed kvstore for working offline - now writing a user record to the storage before testing to ensure there's a record - adjusted tests to handle kvstore returning raw values instead of an object wrapper

--- test.php
+++ test.php
@@ -6,23 +6,38 @@ Code licensed under the BSD License:
 http://github.com/erikeldridge/authproxy/blob/master/license.txt
 */
 
-require 'keydb.php';
-require '../netdb/sdk.php';
+//private
+$users = array(''=>'');
+$netdbKey = '';
 
 //http://github.com/shuber/curl
 require '../curl/curl.php';
 $curl = new Curl;
 
+//BEGIN: set up
+//set up storage for indipendant auditing
+require '../netdb/sdk.php';
+require '../kvstore/interface.php';
+// require '../kvstore/netbd/netdb.php';
+// $storage = new NetDBStore(KeyDB::$netdb_key, KeyDB::$netdb_secret);
+require '../kvstore/sqlite/sqlite.php';
+$storage = new SQLiteStore($netdbKey);
+
+//create user
+$userId = '';
+$storageKey = sprintf('authproxy-users-%s', $userId);
+$value = new stdclass();
+$response = $storage->set($storageKey, json_encode($value));
+
 //BEGIN: insert oauth record
-$userId = 'BG5BMUK24OOYGHWKTJBCX2TN5E';
+$authproxyUrl = sprintf('%s/authproxy/api.php', 'http://localhost/~eldridge');
 $providerName = 'yahoo.com';
 $consumerKey = 'sjdfkjsjdlkfjl';
 $consumerSecret = 'jljsldjfjsldjfljsjdf';
 $callbackUrl = 'http://example.com';
-$url = sprintf('%s/authproxy/api.php', 'http://localhost/~eldridge');
 $params = array(
     'action' => 'insert',
-    'hash' => sha1(KeyDB::$credentials[$userId].$userId),
+    'hash' => sha1($users[$userId].$userId),
     'userId' => $userId,
     'type' => 'oauth',
     'providerName' => $providerName,
@@ -30,17 +45,16 @@ $params = array(
     'consumerSecret' => $consumerSecret,
     'callbackUrl' => $callbackUrl
 );
-$response = json_decode($curl->post($url, $params)->body);
+$response = json_decode($curl->post($authproxyUrl, $params)->body);
 assert('success' == $response->status);
 assert(isset($response->recordId) && !empty($response->recordId));
 
 //audit service record in db
-$storage = new Netdb(KeyDB::$netdb_key, KeyDB::$netdb_secret);
 $recordId = $response->recordId;
 
 $storageKey = sprintf('authproxy-services-%s-%s', $userId, $recordId);
 $response = $storage->get($storageKey);
-$value = json_decode($response->value);
+$value = json_decode($response);
 assert($value->providerName == $providerName);
 assert($value->consumerKey == $consumerKey);
 assert($value->consumerSecret == $consumerSecret);
@@ -49,7 +63,7 @@ assert($value->callbackUrl == $callbackUrl);
 //audit user record in db
 $storageKey = sprintf('authproxy-users-%s', $userId);
 $response = $storage->get($storageKey);
-$value = json_decode($response->value);
+$value = json_decode($response);
 assert(in_array($recordId, $value->recordIds));
 
 //END: insert oauth record
@@ -57,7 +71,7 @@ assert(in_array($recordId, $value->recordIds));
 //get oauth record
 $url = sprintf('%s/authproxy/api.php', 'http://localhost/~eldridge');
 $params = array(
-    'hash' => sha1(KeyDB::$credentials[$userId].$userId),
+    'hash' => sha1($users[$userId].$userId),
     'userId' => $userId,
     'type' => 'oauth',
     
@@ -75,13 +89,13 @@ assert($response->value->callbackUrl == $callbackUrl);
 //BEGIN: delete record
 $url = sprintf('%s/authproxy/api.php', 'http://localhost/~eldridge');
 $params = array(
-    'hash' => sha1(KeyDB::$credentials[$userId].$userId),
+    'hash' => sha1($users[$userId].$userId),
     'userId' => $userId,
     'action' => 'delete',
     'type' => 'oauth',
     'recordId' => $recordId
 );
-$response = json_decode($curl->post($url, $params)->body);var_dump($response);
+$response = json_decode($curl->post($url, $params)->body);
 assert('success' == $response->status);
 
 //audit service record in db
@@ -93,7 +107,7 @@ assert(empty($value));
 //audit user record in db
 $storageKey = sprintf('authproxy-users-%s', $userId);
 $response = $storage->get($storageKey);
-$value = json_decode($response->value);echo "$recordId<br/>";
+$value = json_decode($response);
 assert(!in_array($recordId, $value->recordIds));
 //BEGIN: delete record
 
