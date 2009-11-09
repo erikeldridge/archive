bcd529e3b1833f855d3f0fcd288f234aa19aea7a
erik <erikeldridge@yahoo.com>
2009-11-08 17:32:10 -0800

    added tests to confirm insert or replace was working, and complex values are being accepted

--- sqlite/test.php
+++ sqlite/test.php
@@ -6,7 +6,8 @@ $tableName = 'footable';
 $store = new SQLiteStore($tableName);
 
 //set up
-$store->db->query("CREATE TABLE $tableName (key varchar(100), value varchar(1000), created timestamp(20))", $error);
+//$store->db->query("DROP TABLE $tableName");
+$store->db->query("CREATE TABLE $tableName (key varchar(100) PRIMARY KEY, value varchar(1000), created timestamp(20))", $error);
 
 //get non-existent key
 $key = 'asd123';
@@ -15,6 +16,20 @@ assert(false === $value);
 
 //test insertion
 $value = 'bar';
+$store->set($key, $value);
+$result = $store->get($key);
+assert($value == $result);
+
+//test replace
+$value = 'baz';
+$store->set($key, $value);
+$result = $store->get($key);
+assert($value == $result);
+
+//test json insertion
+$obj = new stdclass();
+$obj->arr = array('this'=>'that');
+$value = json_encode($obj);
 $store->set($key, $value);
 $result = $store->get($key);
 assert($value == $result);
