69b2d78450a1f36ef352bd46d44a1b4678d84483
erik <erikeldridge@yahoo.com>
2009-11-08 17:33:55 -0800

    now checking if table name has leading non-numeric char because sqlite throws non-obvious error for this.  now escaping keys/values passed to sqlite

--- sqlite/sqlite.php
+++ sqlite/sqlite.php
@@ -4,7 +4,14 @@ CREATE TABLE $tableName (key varchar(100), value varchar(1000), created timestam
 */
 class SQLiteStore implements KVStore {
     function __construct($tableName, $filePath = 'sqlite') {
-        $this->tableName = $tableName;
+        $this->tableName = sqlite_escape_string($tableName);
+        if (is_numeric($tableName[0])) {
+            $details = sprintf(
+                "sqlite will choke on table names that start w/ a number.  yours starts w/ '%s'",
+                $tableName[0]
+            );
+            throw new Exception($details);
+        }
         $this->db = new SQLiteDatabase($filePath, 0777, $errorMessage);
     }
     function get($key) {
@@ -25,7 +32,7 @@ class SQLiteStore implements KVStore {
             "REPLACE INTO 
             %s (key, value, created) 
             VALUES ('%s', '%s', '%d');", 
-            $this->tableName, $key, $value, $time
+            $this->tableName, sqlite_escape_string($key), sqlite_escape_string($value), $time
         );
         
         //allow exceptions to bubble up
