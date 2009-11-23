f52f905bce582fc8487189412e1548b20cc7aa32
erik <erikeldridge@yahoo.com>
2009-11-22 23:35:50 -0800

    key, secret, callback, and paths to required libs

---
+++ private.php
@@ -0,0 +1,17 @@
+<?php
+define('OAUTH_CONSUMER_KEY', '');
+define('OAUTH_CONSUMER_SECRET', '');
+define('OAUTH_CALLBACK_URL', '');
+
+//http://github.com/yahoo/yos-social-php5/blob/master/lib/Yahoo/YahooCurl.class.php
+$yahoo_php5_sdk_include_path = dirname(__FILE__).'{path to YahooCurl.class.php}';
+
+//http://github.com/yahoo/yos-social-php5/blob/master/lib/OAuth/OAuth.php
+$oauth_lib_include_path = dirname(__FILE__).'{path to OAuth.php}';
+
+set_include_path(
+    get_include_path().PATH_SEPARATOR
+    .$yahoo_php5_sdk_include_path.PATH_SEPARATOR
+    .$oauth_lib_include_path
+);
+?>
