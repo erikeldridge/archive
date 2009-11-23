d289faab3deb2bcaa3497e8dc6d8287d8094d164
erik <erikeldridge@yahoo.com>
2009-11-22 23:47:23 -0800

    added url for oauth php lib

--- private.php
+++ private.php
@@ -6,7 +6,7 @@ define('OAUTH_CALLBACK_URL', '');
 //http://github.com/yahoo/yos-social-php5/blob/master/lib/Yahoo/YahooCurl.class.php
 $yahoo_php5_sdk_include_path = dirname(__FILE__).'{path to YahooCurl.class.php}';
 
-//http://github.com/yahoo/yos-social-php5/blob/master/lib/OAuth/OAuth.php
+//http://oauth.googlecode.com/svn/code/php/
 $oauth_lib_include_path = dirname(__FILE__).'{path to OAuth.php}';
 
 set_include_path(
