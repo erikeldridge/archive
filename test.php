58c8130106915949fbe4a4243618144f0b6c627a
erik <erikeldridge@yahoo.com>
2009-12-03 22:54:55 -0800

    merged over code from gist-246976

--- test.php
+++ test.php
@@ -1,126 +1,178 @@
 <?php
+error_reporting(E_ALL);
 
-require_once 'private.php';
-require_once 'OAuth.php';
-require_once 'YahooCurl.class.php';
-require_once 'OAuthPanda.class.php';
+require 'private.php';
+require 'OAuthPanda.class.php';
 
-class TestCase {
-    function __construct()
-    {
-        if(method_exists($this, 'setUp')){
-            $this->setUp();
-        }
-        foreach(get_class_methods(get_class($this)) as $method){
-            if(0 === strpos($method, 'test')){
-                try{
-                    $this->{$method}();
-                }catch(Exception $e){
-                    self::dumpPretty($e);
-                }
-            }
-        }
-        if(method_exists($this, 'tearDown')){
-            $this->tearDown();
-        }
-    }
-    
-    static function dumpPretty($value)
-    {
-        printf('<pre>%s</pre><hr/>', print_r($value, true));
-    }
-    
-    function assertEquals($value1, $value2)
-    {
-       if($value1 != $value2){
-           throw(new Exception());
-       }
-    }
+// //test no args
+// new OauthPanda;
 
-    function assertTrue($value)
-    {
-        if(FALSE === $value){
-            throw(new Exception());
-        }
-    }
-}
-class DefaultSettingsTest extends TestCase {
-    
-    function setUp()
-    {        
-        //set up
-        $this->panda = new OAuthPanda(YAHOO_OAUTH_CONSUMER_KEY, YAHOO_OAUTH_CONSUMER_SECRET);
-    }
-    
-    function testConsumerSettings()
-    {
-        // dumpPretty($this->panda->consumer);
-        $this->assertEquals($this->panda->consumer->key, YAHOO_OAUTH_CONSUMER_KEY);
-        $this->assertEquals($this->panda->consumer->secret, YAHOO_OAUTH_CONSUMER_SECRET);
-        $this->assertEquals($this->panda->consumer->callback, NULL);
-    }
-    
-    function testSettings()
-    {
-        $this->assertEquals($this->panda->token, NULL);
-        
-        $this->assertEquals(is_array($this->panda->headers), TRUE);
-        $this->assertEquals(count($this->panda->headers), 0);
-        
-        $this->assertEquals($this->panda->post_params, '');
-        
-        $this->assertEquals($this->panda->oauth_param_location, 'header');
-    }
+//test invalid setting name
+// $foo = new OauthPanda(array(
+//  'blah'=>'baz'
+// ));
+// assert(false);
+
+//test invalid setting value
+// new OauthPanda(array(
+//  'exception_handling'=>'bar'
+// ));
+// assert(false);
+
+//test required setting
+// new OauthPanda(array(
+//  'exception_handling'=> 'print'
+// ));
+// assert(false);
+
+//test default setting value
+$foo = new OauthPanda(array(
+    'request_client' => new stdclass,
+    'oauth_client' => new StandardOauthClient,
+    'consumer_key' => YAHOO_OAUTH_CONSUMER_KEY,
+    'consumer_secret' => YAHOO_OAUTH_CONSUMER_SECRET
+));
+assert('print' == $foo->class_settings['exception_handling']['value']);
+
+//test custom setting value
+$foo = new OauthPanda(array(
+    'exception_handling' => 'throw',
+    'request_client' => new YahooCurlWrapper,
+    'oauth_client' => new StandardOauthClient,
+    'consumer_key' => YAHOO_OAUTH_CONSUMER_KEY,
+    'consumer_secret' => YAHOO_OAUTH_CONSUMER_SECRET
+));
+assert('throw' == $foo->class_settings['exception_handling']['value']);
+
+//test exception actually does get thrown when exception_handling is set to 'throw'
+try {
+    $foo->set(array(
+        'boo'=> 'baz'
+    ));
+} catch (Exception $e) {
+    assert(false !== strpos($e->getMessage(), 'invalid setting name: <i>boo</i>'));
 }
 
-class CustomSettingsTest extends TestCase {
-    function setUp()
-    {        
-        $this->panda = new OAuthPanda(YAHOO_OAUTH_CONSUMER_KEY, YAHOO_OAUTH_CONSUMER_SECRET);
-    }
-    function testSingleSet()
-    {
-        $this->panda->set(array('key' => 'val'));
-        $this->assertEquals($this->panda->key, 'val');
-    }
-    function testMultiSet()
-    {
-        $this->panda->set(array('key1' => 'val1', 'key2' => 'val2'));
-        $this->assertEquals($this->panda->key1, 'val1');
-        $this->assertEquals($this->panda->key2, 'val2');
-    }
+//test setting invalid attribute name using set()
+// $foo->set(array(
+// 'request_client' => (object) array('boo'=>'baz')
+// ));
+// assert(false);
+
+//test setting invalid attribute value using set()
+// $foo->set(array(
+// 'request_client' => array('boo'=>'baz')
+// ));
+// assert(false);
+
+//test chaining set()
+$foo->set(array(
+    'request_client' => (object) array('boo'=>'baz')
+))->set(array(
+    'exception_handling'=> 'print'
+));
+assert('print' == $foo->class_settings['exception_handling']['value']);
+assert('baz' == $foo->class_settings['request_client']['value']->boo);
+
+//test key validation
+$foo = new OauthPanda(array(
+    'exception_handling' => 'throw',
+    'request_client' => new YahooCurlWrapper,
+    'oauth_client' => new StandardOauthClient,
+    'consumer_key' => YAHOO_OAUTH_CONSUMER_KEY,
+    'consumer_secret' => YAHOO_OAUTH_CONSUMER_SECRET
+));
+
+//test bad key type
+try {
+    $foo->set(array('consumer_key'=>array()))->GET(array(
+        'url' => 'https://api.login.yahoo.com/oauth/v2/get_request_token'
+    ));
+    assert(false);
+} catch(Exception $e) {
+    assert(false !== strpos($e->getMessage(), '<i>consumer_key</i> must be a string not: array'));
 }
 
-class RequestTokenTest extends TestCase {
+//test secret validation
+$foo = new OauthPanda(array(
+    'exception_handling' => 'throw',
+    'request_client' => new YahooCurlWrapper,
+    'oauth_client' => new StandardOauthClient,
+    'consumer_key' => YAHOO_OAUTH_CONSUMER_KEY,
+    'consumer_secret' => YAHOO_OAUTH_CONSUMER_SECRET
+));
 
-    function testFetchYahooToken()
-    {
-        $panda = new OAuthPanda(YAHOO_OAUTH_CONSUMER_KEY, YAHOO_OAUTH_CONSUMER_SECRET);
-        $response = $panda->set(array(
-                'oauth_param_location' => 'url'
-            ))->GET(
-                'https://api.login.yahoo.com/oauth/v2/get_request_token', 
-                array('oauth_callback' => OAUTH_CALLBACK_URL)
-            );
-        parse_str($response['response_body'], $data);
-        $this->assertTrue(isset($data['oauth_token']));
-    }
-    
-    function testFetchTwitterToken()
-    {
-        $panda = new OAuthPanda(TWITTER_OAUTH_CONSUMER_KEY, TWITTER_OAUTH_CONSUMER_SECRET);
-        $response = $panda->set(array(
-                'oauth_param_location' => 'url'
-            ))->GET(
-                'http://twitter.com/oauth/request_token', 
-                array('oauth_callback' => OAUTH_CALLBACK_URL)
-            );
-        parse_str($response['response_body'], $data);
-        $this->assertTrue(isset($data['oauth_token']));
-    }
+//test oauth client w/ bad secret type --> fail
+try {
+    $foo->set(array('consumer_secret'=>array()))->GET(array(
+        'url' => 'https://api.login.yahoo.com/oauth/v2/get_request_token'
+    ));
+    assert(false);
+} catch(Exception $e) {
+    assert(false !== strpos($e->getMessage(), '<i>consumer_secret</i> must be a string not: array'));
 }
 
-$test = new DefaultSettingsTest();
-$test = new CustomSettingsTest();
-$test = new RequestTokenTest();
+//test called method name validation w/ incorrect method name --> fail
+// $foo->BAR(array(
+//  'url' => 'blah',
+// 'key' => 'qwe123'
+// ));
+
+//test called method name validation w/ deliberate method name --> pass
+$foo->set(array('request_client' => new YahooCurlWrapper))->BAR(array(
+    'request_method' => 'GET',
+    'url' => 'https://api.login.yahoo.com/oauth/v2/get_request_token',
+));
+
+// test method input name validation w/ invalid value
+// $foo->GET(array());
+
+//test correct method input name validation w/ valid value
+$foo->GET(array(
+    // 'url' => 'https://api.login.yahoo.com/oauth/v2/get_request_token',
+    'url' => 'https://api.login.yahoo.com/oauth/v2/get_request_token',
+    'params' => array('oauth_callback' => OAUTH_CALLBACK_URL)
+));
+
+//BEGIN: standard oauth client tests
+//test oauth client w/ no scheme in url --> fail
+// $foo->GET(array(
+//     // 'url' => 'https://api.login.yahoo.com/oauth/v2/get_request_token',
+//     'url' => 'localhost/~eldridge/gist-246976/provider.php',
+//     'params' => array('oauth_callback' => OAUTH_CALLBACK_URL)
+// ));
+
+//test oauth client w/ non-(http|https) scheme --> pass
+$foo->GET(array(
+    'url' => 'zzz://localhost/~eldridge/gist-246976/provider.php',
+    'params' => array('oauth_callback' => OAUTH_CALLBACK_URL)
+));
+
+//test oauth client w/ bad url --> fail
+// $foo->GET(array(
+//     'url' => 'http//ovider.php'
+// ));
+
+//test correct response from yahoo
+$foo = new OauthPanda(array(
+    'exception_handling' => 'throw',
+    'request_client' => new YahooCurlWrapper,
+    'oauth_client' => new StandardOauthClient,
+    'consumer_key' => YAHOO_OAUTH_CONSUMER_KEY,
+    'consumer_secret' => YAHOO_OAUTH_CONSUMER_SECRET
+));
+$response = $foo->GET(array(
+    'url' => 'https://api.login.yahoo.com/oauth/v2/get_request_token',
+    'params' => array('oauth_callback' => OAUTH_CALLBACK_URL)
+));
+assert(false !== strpos($response['response_body'], 'oauth_token='));
+
+
+printf('<pre>%s</pre>', print_r($response, true));
+
+//test correct method input value validation
+// $foo->BAR(array(
+//  'url' => array()
+// ));
+
 ?>
