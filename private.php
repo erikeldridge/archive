35890f30f4d6de38e1b28d4d4f366080d3c49a29
erik <erikeldridge@yahoo.com>
2009-12-03 23:29:52 -0800

    removed include path adjustments for now

--- private.php
+++ private.php
@@ -9,15 +9,4 @@ define('TWITTER_OAUTH_CONSUMER_SECRET', '');
 
 define('OAUTH_CALLBACK_URL', '');
 
-//http://github.com/yahoo/yos-social-php5/blob/master/lib/Yahoo/YahooCurl.class.php
-$yahoo_php5_sdk_include_path = dirname(__FILE__).'{path to YahooCurl.class.php}';
-
-//http://oauth.googlecode.com/svn/code/php/
-$oauth_lib_include_path = dirname(__FILE__).'{path to OAuth.php}';
-
-set_include_path(
-    get_include_path().PATH_SEPARATOR
-    .$yahoo_php5_sdk_include_path.PATH_SEPARATOR
-    .$oauth_lib_include_path
-);
 ?>
