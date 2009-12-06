ee83f2c1b858e060399df4c68963bd85271f7c62
erik <erikeldridge@yahoo.com>
2009-12-06 01:44:04 -0800

    defined wrapper for shuber's curl lib

--- OauthPanda.class.php
+++ OauthPanda.class.php
@@ -139,6 +139,66 @@ class YahooCurlWrapper implements HttpRequestWrapper
     }
 }
 
+class ShuberCurlWrapper implements HttpRequestWrapper
+{
+    function __construct($include_path=false)
+    {
+        $this->include_path = $include_path;
+    }
+    
+    function request($request_method, Array $headers, $url, $param_string)
+    {
+        // check for dependencies when request() is called so oauthpanda can catch the exception
+        if (false === $this->include_path || false === is_file($this->include_path)) {
+            $message = '<p>Shuber\'s <i>Curl</i> client library is required. You can get it here:<br/>'
+                .'<i><a href="http://github.com/shuber/curl">'
+                .'http://github.com/shuber/curl'
+                .'</a></i><br/>'
+                .'Pass the location of the base file into the ShuberCurlWrapper constructor, e.g.,<br/>'
+                .'<i>\'request_client\' => new ShuberCurlWrapper(\'../../curl/curl.php\'),</i><br/>';
+            throw new Exception($message);
+        }
+    
+        require_once $this->include_path;
+    
+        //terse enforcement of types because we're not interfacing w/ user
+        assert(is_string($url));
+        assert(is_string($param_string) || is_null($param_string));
+    
+        //for now, only support get and post
+        assert(is_string($request_method) && in_array($request_method, array('GET', 'POST')));
+    
+        $curl = new Curl;
+        
+        switch ($request_method) {
+            case 'GET':
+        
+                //append params to url, so they aren't encoded in a non-oauth-compliant way inside lib
+                $url .= '?'.$param_string;
+                
+                $response = $curl->get($url);
+                
+                break;
+            case 'POST':
+            
+                // reformat param pairs back into assoc array
+                $param_pairs = explode('&', $param_string);
+                $params = array();
+                foreach ($param_pairs as $pair) {
+                    list($name, $value) = explode('=', $pair);
+                    $params[$name] = $value;
+                }
+                
+                $response = $curl->post($url, $params);
+                break;
+            default:
+                break;
+        }
+    
+        return $response;
+    }
+}
+
 //the standard exception class truncates the message string
 class VerboseException extends Exception
 {
