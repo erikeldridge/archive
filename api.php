8803114d89c97ac5a446e2f1c4c31003ddebeca2
erik <erikeldridge@yahoo.com>
2009-12-06 21:24:21 -0800

    moved tests to their own file, and now setting content-type header

--- api.php
+++ api.php
@@ -1,17 +1,10 @@
 <?php
-class Runner
-{
-    function test1 ()
-    {
-        $result = (object) array(
-            'id' => $_GET['test'],
-            'result' => 'pass'
-        );
-        $json = json_encode($result);
-        echo "callback($json);";
-    }
-}
 
-$runner = new Runner;
-$runner->{$_GET['test']}();
+require 'tests.php';
+
+header('Content-Type: application/javascript');
+
+$tests = new Tests;
+$tests->{$_GET['test']}();
+
 ?>
