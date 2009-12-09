5ddc06f2ab9d03f7b9f1492d0523a886a1c7e886
erik <erikeldridge@yahoo.com>
2009-12-09 08:05:03 -0800

    added license text

--- OauthWrapper.php
+++ OauthWrapper.php
@@ -1,5 +1,33 @@
 <?php
 
+/*
+OAuthPanda
+
+* package: http://github.com/erikeldridge/oauthpanda
+* author: Erik Eldridge
+* copyright: Copyrights for code authored by Erik Eldridge is licensed under the following terms:
+* license: BSD Open Source License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
 interface OauthWrapper
 {
     //not static because we can't call static methods from objects assigned to arrays in php 5.2
