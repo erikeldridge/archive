acdea35be4e41a98794ef0b13551bbaf780d7412
erik <erikeldridge@yahoo.com>
2010-04-22 11:22:39 -0700

    now refreshing assoc

--- _openid_start.php
+++ _openid_start.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'SqliteStore.php';
 
 function yql( $query )
@@ -30,8 +31,9 @@ $assoc_store = new SqliteStore('assoc');
 $assoc_db_key = md5( $input['key'].$input['id'] );
 $assoc_json = $assoc_store->get( $assoc_db_key );
 
-//get assoc if it's not in the db
-if ( !$assoc_json ) {
+//get assoc if it's not in the db, or if it's expired
+$time = time();
+if ( !$assoc_json || json_decode( $assoc_json )->expires < $time ) {
     $assoc_query = sprintf( 
         "use 'http://gist.github.com/yql/yql-tables/raw/master/openid/openid.assoc.xml' as openid.assoc;"
         ."use 'http://gist.github.com/yql/yql-tables/raw/master/openid/openid.discover.xml' as openid.discover;"
@@ -40,6 +42,7 @@ if ( !$assoc_json ) {
     );
     $assoc_results = yql( $assoc_query );
     $assoc = $assoc_results->query->results->success;
+    $assoc->expires = $time + $assoc->expires_in;
     $assoc_store->set( $assoc_db_key, json_encode( $assoc ) );
 } else {
    $assoc = json_decode( $assoc_json );
@@ -48,11 +51,11 @@ if ( !$assoc_json ) {
 //fetch openid log in url
 $login_query = sprintf( 
     "use 'http://gist.github.com/yql/yql-tables/raw/master/openid/openid.xml' as openid;"
-    ."select * from openid where id = '%s' and return_to = '%s' and assoc_handle = '%s'", 
-    $input['id'], urldecode( $input['return_to'] ), $assoc->handle 
+    ."select * from openid where id = '%s' and return_to = '%s' and assoc_handle = '%s' and oauthKey = '%s'", 
+    $input['id'], urldecode( $input['return_to'] ), $assoc->assoc_handle, $input['key']
 );
 $login_results = yql( $login_query );
 
 //redirect to log in
-header( "Location: ".$login_results->query->results->success );
+header( 'Location: '.$login_results->query->results->success );
 ?>
