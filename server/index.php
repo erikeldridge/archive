b8f29ecc9e7f60e99e7ea4a246ebce0d64a0bd09
erik <erikeldridge@yahoo.com>
2010-05-13 21:13:56 -0700

    updated header comment block project-wide

--- server/index.php
+++ server/index.php
@@ -1,29 +1,9 @@
 <?php
-
 /**
- * @package    http://github.com/erikeldridge/foxbat/tree/master
- * @copyright  (c) 2009, Erik Eldridge, all rights reserved
- * @license    BSD Open Source License
- *
- *   Permission is hereby granted, free of charge, to any person obtaining a copy
- *   of this software and associated documentation files (the "Software"), to deal
- *   in the Software without restriction, including without limitation the rights
- *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
- *
- *   The above copyright notice and this permission notice shall be included in
- *   all copies or substantial portions of the Software.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *   THE SOFTWARE.
+ * Source: http://github.com/erikeldridge/iframeio/tree/master
+ * Copyright: (c) 2009, Erik Eldridge, all rights reserved
+ * License: BSD Open Source License http://gist.github.com/375593
  **/
-
 function respond( $data, $id, $xdrUrl ){
     
     //format data for output
