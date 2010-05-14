98e151ccde9fe1a28f389f086a949b28b39bfce5
erik <erikeldridge@yahoo.com>
2010-05-13 19:15:56 -0700

    now requiring ck, fetching xdrurl from demo storage, and responding in a structured way

--- server/index.php
+++ server/index.php
@@ -23,14 +23,29 @@
  *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *   THE SOFTWARE.
  **/
- 
-// xdr url should be set using stored association w/ app-id/oauth-key passed in
-$xdrUrl = 'http://localhost/~eldridge/github/erikeldridge/iframeio/client/iframe.html';
+
+function respond( $data, $id, $xdrUrl ){
+    
+    //format data for output
+    $data = urlencode(json_encode($data));
+
+    //split output into chunks of arbitrary size (pending length avail. for GET params)
+    $size = 2000;
+    $chunks = str_split($data, $size);
+    $total = count($chunks);
+    
+    header('Content-Type: text/html');
+    
+    foreach($chunks as $index => $chunk){
+        printf( '<iframe src="%s?id=%s&index=%s&total=%d&chunk=%s"></iframe>', $xdrUrl, $id, $index, $total, $chunk );
+    }
+    
+    exit;
+}
 
 //fetch input
 $filters = array(
-    
-    //request params
+    'oauthConsumerKey' => FILTER_SANITIZE_STRING,
     'method' => FILTER_SANITIZE_STRING,
     'url' => FILTER_SANITIZE_STRING,
     'params' => FILTER_SANITIZE_STRING,
@@ -40,9 +55,24 @@ $filters = array(
 );
 $input = filter_var_array($_GET, $filters);
 
-//validate input
+// if we don't have a frame id, the whole response mechanism is broken.  
+// this should only happen for completely invalid requests
 if(!isset($input['id'])){
-    $data = array('error'=>"iframe id req'd for all requests");    
+    header('Content-Type: text/plain');
+    die( "iframe id req'd for all requests" );
+}
+
+// xdr url should be set using stored association w/ app-id/oauth-key passed in
+$apps = array(
+    'asd123' => array(
+        'xdrUrl' => 'http://localhost/~eldridge/github/erikeldridge/iframeio/client/iframe.html'
+    )
+);
+
+// if we don't have an app defined for ck, return error
+if (!isset($apps[ $input['oauthConsumerKey'] ]['xdrUrl'])) {
+    header('Content-Type: text/plain');
+    die( 'app undefined for oauthConsumerKey: '.$input['oauthConsumerKey'] );
 }
 
 //sample request handler
@@ -56,19 +86,8 @@ if('get' == $input['method']){
     
     //format response for json encode
     $data = array('response'=>$response);
+    
+    respond( $data, $input['id'], $apps[ $input['oauthConsumerKey'] ]['xdrUrl'] ); 
 }
 
-//format data for output
-$data = urlencode(json_encode($data));
-
-//split output into chunks of arbitrary size (pending length avail. for GET params)
-$size = 100;
-$chunks = str_split($data, $size);
-$total = count($chunks);
-
-//output markup
 ?>
-
-<? foreach($chunks as $index => $chunk): ?>
-    <iframe src="<?= $xdrUrl ?>?id=<?= $input['id'] ?>&index=<?= $index ?>&total=<?= $total ?>&chunk=<?= $chunk ?>"></iframe>
-<? endforeach ?>
