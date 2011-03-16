bcd574165ca7dcb9bcd410ccb84731141f22ab6b
Erik Eldridge <erik@twitter.com>
2011-03-16 00:18:41 -0700

    Use delegate & left-click checking in router
    Follow http://mislav.uniqpath.com/2011/03/click-hijack/
    and use delegate() to assign click handling,
    check filter out non-left clicks, and return false
    to cancel bubble and default action.

--- static/routing.js
+++ static/routing.js
@@ -1,8 +1,12 @@
-$('.routed').click(function(e){
-  var controller = e.target.href.split('/').pop();
+$('body').delegate('.routed', 'click', function(e){
+  // http://mislav.uniqpath.com/2011/03/click-hijack/
+  if(e.which != 1 || e.metaKey || e.shiftKey){
+    return;
+  }
   if(!window.history || !window.history.pushState){
     return;
   }
+  var controller = e.target.href.split('/').pop();
   window[controller]();
-  e.preventDefault();
+  return false;
 });
