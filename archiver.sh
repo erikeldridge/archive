1f20d1f702f23151a1769404d3c733029ebaa579
Erik Eldridge <erik@twitter.com>
2011-11-27 04:34:28 -0800

    Create project dir & move merged files into it

--- archiver.sh
+++ archiver.sh
@@ -9,17 +9,61 @@ fi
 # Extract project name from repo url
 project_name=$( ruby -e "print '$1'.scan(/(\w+)\.git/)[0][0]" );
 
-# Clone project
+echo 'Cloning project';
 git clone $1 ../$project_name;
 
-# Define remote pointing at local copy of project
-git remote rm merger
-git remote add merger ../$project_name;
+echo 'Deleting merge_branch';
+git branch -D merge_branch;
+git reset --hard ;
 
-# Merge in project
-git pull merger master
+echo 'Creating merge_branch';
+git co -b merge_branch;
 
-# Make directory to house project files
-mkdir $project_name
+echo 'Defining remote pointing at local copy of project';
+git remote rm merge_remote;
+git remote add merge_remote ../$project_name;
 
+echo 'Merging in project';
+git pull merge_remote master;
 
+echo 'Making directory to house project files';
+mkdir $project_name;
+
+echo 'Identifying new files:';
+file_names=`git diff master --name-only`
+echo $file_names;
+
+echo 'Moving files into project dir'
+for f in $file_names
+do 
+
+  # Skip this file if it's in the list
+  if [ $f = `basename $0` ]
+  then
+    continue
+  fi
+
+  # If the file is nested, only move the parent dir ...
+  dir_name=`dirname $f`
+  if [ $dir_name = '.' ]
+  then
+    mv "$f" "$project_name"
+
+  # ... unless the parent dir is already defined.
+  elif [ ! -d $project_name/$dir_name ]
+  then
+    mv "$dir_name" "$project_name";
+  fi
+
+done
+
+echo 'Committing changes to merge_branch' 
+git add $project_name
+git ci -am "Create $project_name dir & add files"
+
+echo 'Merging merge_branch into master'
+git co master
+git merge merge_branch
+
+echo 'Pushing to github'
+git push github master
