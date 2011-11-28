eddd3cf23bc1a70808984860372289dfd20dceb3
Erik Eldridge <erik@twitter.com>
2011-11-27 21:17:39 -0800

    Create archive_branch for staging archival

--- archive_project.sh
+++ archive_project.sh
@@ -2,12 +2,12 @@
 
 # Require project repo url as 1st arg
 if [ ! $1 ]; then
-  echo 'project url required, e.g., $ bash archiver.sh git@github.com:erikeldridge/CivicDB.git';
-  exit 1;
+  echo "$0: project url required, e.g., $ bash archiver.sh git@github.com:erikeldridge/CivicDB.git"
+  exit 1
 fi
 
 # Extract project name from repo url
-project_name=$( ruby -e "print '$1'.scan(/(\w+)\.git/)[0][0]" );
+project_name=$( ruby -e "print '$1'.scan(/([\w-]+)\.git/)[0][0]" );
 
 echo "$0: Cloning project";
 git clone $1 ../$project_name;
@@ -15,13 +15,18 @@ git clone $1 ../$project_name;
 echo "$0: Changing directory to ../$project_name";
 cd ../$project_name
 
-archive_dir="${project_name}_archive"
+echo "$0: Making archive_branch";
+git co -b archive_branch
+
+archive_dir="project_archive"
 mkdir $archive_dir;
 echo "$0: Making '$archive_dir' directory to house project files";
 
 echo "$0: Moving files into archive directory";
 for f in *
 do
+  
+  # Skip archive dir itself
   if [ $f = $archive_dir ]
   then
     continue
@@ -37,17 +42,13 @@ git ci -am "Create $archive_dir dir & add files"
 echo "$0: Changing directory back to archive project";
 cd -
 
-echo "$0: Creating merge_branch";
-git co -b merge_branch;
-
-echo "$0: Defining remote pointing at local copy of project";
-git remote rm merge_remote;
+echo "$0: Defining remote repository pointing at local project";
 git remote add merge_remote ../$project_name;
 
 echo "$0: Merging in project";
-git pull merge_remote master;
+git pull merge_remote archive_branch;
 
-echo "$0: Removing '_archive' from archive directory name"
+echo "$0: Renaming archive directory to repository name"
 if [ -d $project_name ]
   then
   echo "$0: Error: directory with this name already exists"
@@ -57,11 +58,13 @@ mv $archive_dir $project_name
 
 echo "$0: Committing changes to merge_branch"
 git add $project_name
-git ci -am "Remove '_archive' from archive directory name"
+git ci -am "Renaming archive directory to repository name"
 
-echo "$0: Merging merge_branch into master"
+echo "$0: Deleting remote definition";
+git remote rm merge_remote;
+
+echo "$0: Deleting archive branch"
+cd -
 git co master
-git merge merge_branch
-
-echo "$0: Deleting merge_branch";
-git branch -d merge_branch;
+git branch -d archive_branch
+cd -
