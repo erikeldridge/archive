c8d717592e0584383c2fb1bc366a7aef9d0416ce
Erik Eldridge <erik@twitter.com>
2011-11-27 21:25:25 -0800

    Use 'repository' instead of 'project'

--- archive_project.sh
+++ archive_project.sh
@@ -1,26 +1,26 @@
-# This script accepts a github repo url, clones it, and merges it into the archive project
+# This script accepts a github repo url, clones it, and merges it into the archive repository
 
 # Require project repo url as 1st arg
 if [ ! $1 ]; then
-  echo "$0: project url required, e.g., $ bash archiver.sh git@github.com:erikeldridge/CivicDB.git"
+  echo "$0: repository url required, e.g., $ bash archiver.sh git@github.com:erikeldridge/CivicDB.git"
   exit 1
 fi
 
-# Extract project name from repo url
-project_name=$( ruby -e "print '$1'.scan(/([\w-]+)\.git/)[0][0]" );
+# Extract repository name from url
+repository_name=$( ruby -e "print '$1'.scan(/([\w-]+)\.git/)[0][0]" );
 
-echo "$0: Cloning project";
-git clone $1 ../$project_name;
+echo "$0: Cloning repository";
+git clone $1 ../$repository_name;
 
-echo "$0: Changing directory to ../$project_name";
-cd ../$project_name
+echo "$0: Changing directory to ../$repository_name";
+cd ../$repository_name
 
 echo "$0: Making archive_branch";
 git co -b archive_branch
 
-archive_dir="project_archive"
+archive_dir="repository_archive"
 mkdir $archive_dir;
-echo "$0: Making '$archive_dir' directory to house project files";
+echo "$0: Making '$archive_dir' directory to house repository files";
 
 echo "$0: Moving files into archive directory";
 for f in *
@@ -39,25 +39,25 @@ echo "$0: Committing archive directory";
 git add $archive_dir
 git ci -am "Create $archive_dir dir & add files"
 
-echo "$0: Changing directory back to archive project";
+echo "$0: Changing directory back to archive repository";
 cd -
 
-echo "$0: Defining remote repository pointing at local project";
-git remote add merge_remote ../$project_name;
+echo "$0: Defining remote repository pointing at ../$repository_name";
+git remote add merge_remote ../$repository_name;
 
-echo "$0: Merging in project";
+echo "$0: Merging in repository";
 git pull merge_remote archive_branch;
 
 echo "$0: Renaming archive directory to repository name"
-if [ -d $project_name ]
+if [ -d $repository_name ]
   then
   echo "$0: Error: directory with this name already exists"
   exit 1
 fi
-mv $archive_dir $project_name
+mv $archive_dir $repository_name
 
 echo "$0: Committing changes to merge_branch"
-git add $project_name
+git add $repository_name
 git ci -am "Renaming archive directory to repository name"
 
 echo "$0: Deleting remote definition";
